be0d03f89b785c8dacc695d8bac9285503b54f14
bniepce <brad.niepce@kaptios.com>
2024-12-02 14:52:51 +0100

    Add requirements.txt

--- src/utils/augmentation.py
+++ src/utils/augmentation.py
@@ -6,38 +6,6 @@ from glob import glob
 from tqdm import tqdm
 
 
-def make_augmentation(data_dir, num_augmentations: int = 3):
-
-    logging.info("Running data augmentation")
-
-    augmentation_pipeline = A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.VerticalFlip(p=0.5),
-        A.Rotate(limit=45, border_mode=cv2.BORDER_CONSTANT, p=0.3),
-        A.ElasticTransform(alpha=1, sigma=50, p=0.3),
-        A.RandomBrightnessContrast(
-            brightness_limit=0.2, contrast_limit=0.2, p=0.5),
-        A.GaussNoise(var_limit=(10.0, 50.0), p=0.3),
-        A.GaussianBlur(blur_limit=(3, 7), p=0.3),
-        A.RandomGamma(gamma_limit=(80, 120), p=0.4),
-        A.RandomResizedCrop(height=224, width=224, scale=(0.9, 1.0), p=0.5),
-    ])
-
-    label_folders = glob(os.path.join(data_dir, '*'))
-
-    for label in label_folders:
-        number_of_images = glob(label + '/*.png')
-        with tqdm(total=len(number_of_images), desc=f"Augmenting {label} images") as pbar:
-            for i, img in enumerate(number_of_images):
-                augmented_images = augment_image(img)
-                for j, augmented_image in enumerate(augmented_images):
-                    output_path = f"{label}/aug_{i}_{j}.png"
-                    cv2.imwrite(output_path, augmented_image)
-                pbar.update()
-
-    logging.info("Augmentations finished.")
-
-
 def augment_image(image_path, n_augment, pipeline):
     image = cv2.imread(image_path)
     augmented_images = []
@@ -60,7 +28,6 @@ def make_classwise_augmentations(data_dir, n_augment, class_list=['mass', 'suspi
         A.GaussNoise(var_limit=(10.0, 50.0), p=0.3),
         A.GaussianBlur(blur_limit=(3, 7), p=0.3),
         A.RandomGamma(gamma_limit=(80, 120), p=0.4),
-        A.RandomResizedCrop(height=224, width=224, scale=(0.9, 1.0), p=0.5),
     ])
 
     class_dirs = [os.path.join(data_dir, i) for i in class_list]
